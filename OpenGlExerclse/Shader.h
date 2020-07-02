378d42346b9ddcb08c5f6f6195c7c4bf1bbbba95
EternalWhiteShrimp <46107217+EternalWhiteShrimp@users.noreply.github.com>
2020-07-02 09:57:13 +0800

    Add files via upload
    c代码和debug

---
+++ OpenGlExerclse/Shader.h
@@ -0,0 +1,218 @@
+#pragma  once
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include<GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+public:
+	unsigned int ID;
+
+	Shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath = nullptr)
+	{
+		std::string vertexCode;
+		std::string fragmentCode;
+		std::string geometryCode;
+
+		std::ifstream vShaderFile;
+		std::ifstream fShaderFile;
+		std::ifstream gShaderFile;
+
+		vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		gShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+		try
+		{
+			vShaderFile.open(vertexPath);
+			fShaderFile.open(fragmentPath);
+			std::stringstream vShaderStream, fShaderStream;
+
+			vShaderStream << vShaderFile.rdbuf();
+			fShaderStream << fShaderFile.rdbuf();
+
+			vShaderFile.close();
+			fShaderFile.close();
+
+			vertexCode = vShaderStream.str();
+			fragmentCode = fShaderStream.str();
+
+
+			if (geometryPath != nullptr)
+			{
+				gShaderFile.open(geometryPath);
+				std::stringstream gShaderStream;
+				gShaderStream << gShaderFile.rdbuf();
+				gShaderFile.close();
+				geometryCode = gShaderStream.str();
+			}
+		}
+		catch (std::ifstream::failure e)
+		{
+			std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
+		}
+
+		const char* vShaderCode = vertexCode.c_str();
+		const char* fShaderCode = fragmentCode.c_str();
+
+		// compile shaders
+		unsigned int vertex, fragment;
+		int success;
+		char infoLog[512];
+
+
+		GLuint vertexId, fragmentId;
+		//vertexId = glCreateShader(GL_VERTEX_SHADER);
+		//glShaderSource(vertexId, 1, &vertexSourceC, NULL);
+		//glCompileShader(vertexId);
+		//CheckCompileErrors(vertexId, "VERTEXSHADER");
+		//fragmentId = glCreateShader(GL_FRAGMENT_SHADER);
+		//glShaderSource(fragmentId, 1, &fragmentSourceC, NULL);
+		//glCompileShader(fragmentId);
+		//CheckCompileErrors(fragmentId, "FRAGMENTSHADER");
+
+
+		// vertex shader
+		vertex = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertex, 1, &vShaderCode, NULL);
+		glCompileShader(vertex);
+		checkCompileErrors(vertex, "VERTEX");
+
+		//fragment shader
+		fragment = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragment, 1, &fShaderCode, NULL);
+		glCompileShader(fragment);
+		checkCompileErrors(fragment, "FRAGMENT");
+
+		unsigned int geometry;
+		if (geometryPath != nullptr)
+		{
+			const char* gShaderCode = geometryCode.c_str();
+			geometry = glCreateShader(GL_GEOMETRY_SHADER);
+			glShaderSource(geometry, 1, &gShaderCode, NULL);
+			glCompileShader(geometry);
+			checkCompileErrors(geometry, "GEOMETRY");
+		}
+
+		ID = glCreateProgram();
+		glAttachShader(ID, vertex);
+		glAttachShader(ID, fragment);
+		if (geometryPath != nullptr)
+		{
+			glAttachShader(ID, geometry);
+		}
+		glLinkProgram(ID);
+		checkCompileErrors(ID, "PROGRAM");
+
+		// delete the shaders as they're linked into our program now and no longer necessery
+		glDeleteShader(vertex);
+		glDeleteShader(fragment);
+
+		if (geometryPath != nullptr)
+			glDeleteShader(geometry);
+	}
+
+
+	void Use()
+	{
+		glUseProgram(ID);
+	}
+
+	void SetBool(const std::string& Name, bool Value) const
+	{
+		glUniform1i(glGetUniformLocation(ID, Name.c_str()), (int)Value);
+	}
+
+	void SetInt(const std::string& Name, int Value) const
+	{
+		glUniform1i(glGetUniformLocation(ID, Name.c_str()), Value);
+	}
+
+	void SetFloat(const std::string& Name, float Value) const
+	{
+		glUniform1f(glGetUniformLocation(ID, Name.c_str()), Value);
+	}
+
+	void SetVec2(const std::string& Name, const glm::vec2& Value) const
+	{
+		glUniform2fv(glGetUniformLocation(ID, Name.c_str()), 1, &Value[0]);
+	}
+
+	void SetVec2(const std::string& Name, float x, float y) const
+	{
+		glUniform2f(glGetUniformLocation(ID, Name.c_str()), x, y);
+	}
+
+	void SetVec3(const std::string& Name, const glm::vec3& Value) const
+	{
+		glUniform3fv(glGetUniformLocation(ID, Name.c_str()), 1, &Value[0]);
+	}
+
+	void SetVec3(const std::string& Name, float x, float y, float z) const
+	{
+		glUniform3f(glGetUniformLocation(ID, Name.c_str()), x, y, z);
+	}
+
+	void SetVec4(const std::string& Name, glm::vec4& Value) const
+	{
+		glUniform4fv(glGetUniformLocation(ID, Name.c_str()), 1, &Value[0]);
+	}
+
+	void SetVec4(const std::string& Name, float x, float y, float z, float w) const
+	{
+		glUniform4f(glGetUniformLocation(ID, Name.c_str()), x, y, z, w);
+	}
+
+	void SetMat2(const std::string& Name, const glm::mat2& Mat) const
+	{
+		glUniformMatrix2fv(glGetUniformLocation(ID, Name.c_str()), 1, GL_FALSE, &Mat[0][0]);
+	}
+
+	void SetMat3(const std::string& Name, const glm::mat3& Mat) const
+	{
+		glUniformMatrix3fv(glGetUniformLocation(ID, Name.c_str()), 1, GL_FALSE, &Mat[0][0]);
+	}
+
+	void SetMat4(const std::string& Name, const glm::mat4& Mat) const
+	{
+		glUniformMatrix4fv(glGetUniformLocation(ID, Name.c_str()), 1, GL_FALSE, &Mat[0][0]);
+	}
+
+private:
+
+	void checkCompileErrors(GLuint Shader, std::string Type)
+	{
+		GLint success;
+		GLchar infoLog[1024];
+
+		if (Type != "PROGRAM")
+		{
+			glGetShaderiv(Shader, GL_COMPILE_STATUS, &success);
+			if (!success)
+			{
+				glGetShaderInfoLog(Shader, 1024, NULL, infoLog);
+				std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << Type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+			}
+
+		}
+		else
+		{
+			glGetProgramiv(Shader, GL_LINK_STATUS, &success);
+			if (!success)
+			{
+				glGetProgramInfoLog(Shader, 1024, NULL, infoLog);
+				std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << Type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+
+			}
+		}
+	}
+
+};
+
---
+++ OpenGlExerclse/main.cpp
@@ -0,0 +1,232 @@
+//#include "stdafx.h"
+
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+#include"Camera.h"
+#include"Shader.h"
+#include"Model.h"
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool bMouseDown = false;
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+glm::vec3 lightPos(0.f, 0.0f, 20.f);
+
+int main()
+{
+
+	// glfw: initialize and configure
+	// ------------------------------
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glewExperimental = GL_TRUE;
+	//// Initialize GLEW to setup the OpenGL Function pointers
+	glewInit();
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetMouseButtonCallback(window, mouse_button_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	// tell GLFW to capture our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// glad: load all OpenGL function pointers
+	// ---------------------------------------
+	/*if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}*/
+
+	// configure global opengl state
+	// -----------------------------
+	glEnable(GL_DEPTH_TEST);
+
+	// build and compile shaders
+	// -------------------------
+	Shader ourShader("F:\\OpenGlProcedure\\OpenGlExerclse\\Shader\\vertex.vert", "F:\\OpenGlProcedure\\OpenGlExerclse\\Shader\\fragment.frag");
+
+	Model ourModel("F:\\OpenGlProcedure\\OpenGlExerclse\\Debug\\Model\\nanosuit.obj");
+	//Model ourModel("../resources/objects/cyborg/cyborg.obj");
+
+	// draw in wireframe
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	// render loop
+	// -----------
+	while (!glfwWindowShouldClose(window))
+	{
+		// per-frame time logic
+		// --------------------
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		// input
+		// -----
+		processInput(window);
+
+		// render
+		// ------
+		glClearColor(0.3f, 0.35f, 0.35f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// don't forget to enable shader before setting uniforms
+		ourShader.Use();
+
+		// view/projection transformations
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		glm::mat4 view = camera.GetViewMatrix();
+		ourShader.SetMat4("projection", projection);
+		ourShader.SetMat4("view", view);
+
+		ourShader.SetVec3("viewPos", camera.Position);
+		ourShader.SetVec3("lightPos", lightPos);
+
+		//ourShader.SetVec3("light.direction", -0.2f, -1.0f, -0.3f);
+		//ourShader.SetVec3("viewPos", camera.Position);
+
+		// light properties
+		//ourShader.SetVec3("light.ambient", 0.4f, 0.4f, 0.4f);
+		//ourShader.SetVec3("light.diffuse", 0.6f, 0.6f, 0.6f);
+		//ourShader.SetVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
+		// material properties
+		//ourShader.SetFloat("shininess", 32.0f);
+
+
+		// render the loaded model
+		glm::mat4 model;
+		model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // translate it down so it's at the center of the scene
+		model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));	// it's a bit too big for our scene, so scale it down
+		ourShader.SetMat4("model", model);
+		ourModel.Draw(ourShader);
+
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// ------------------------------------------------------------------
+	glfwTerminate();
+
+	return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (bMouseDown)
+	{
+		float xoffset = xpos - lastX;
+		float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+		lastX = xpos;
+		lastY = ypos;
+		camera.ProcessMouseMovement(xoffset, yoffset);
+	}
+
+	lastX = xpos;
+	lastY = ypos;
+
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+	if (action == GLFW_PRESS) switch (button)
+	{
+	case GLFW_MOUSE_BUTTON_LEFT:
+		bMouseDown = true;
+		break;
+	case GLFW_MOUSE_BUTTON_MIDDLE:
+
+		break;
+	case GLFW_MOUSE_BUTTON_RIGHT:
+
+		break;
+	default:
+		return;
+	}
+	else if (action == GLFW_RELEASE) switch (button)
+	{
+	case GLFW_MOUSE_BUTTON_LEFT:
+		bMouseDown = false;
+		break;
+	case GLFW_MOUSE_BUTTON_MIDDLE:
+
+		break;
+	case GLFW_MOUSE_BUTTON_RIGHT:
+
+		break;
+	default:
+		return;
+	}
+	return;
+}
